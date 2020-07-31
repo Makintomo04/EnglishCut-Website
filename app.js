0446f455283c55c1b75ac48075c86b04ea481e6a
Makintomo04 <1721809@alumni.brunel.ac.uk>
2020-07-31 03:18:44 +0100

    site redesign + animations

--- app.js
+++ app.js
@@ -21,3 +21,42 @@ backToTop.addEventListener("mouseover", () => {
 backToTop.addEventListener("mouseout", () => {
   bttArrow.classList.remove("greyarrow");
 });
+
+const sliderImg = document.querySelector(".slider-img");
+const images = ["insta-1.jpg", "insta-2.jpg", "insta-3.jpg", "insta-4.jpg"];
+let i = 0;
+
+function prev() {
+  if (i <= 0) i = images.length;
+  i--;
+  return setImg();
+}
+function next() {
+  if (i >= images.length - 1) i = -1;
+  i++;
+  return setImg();
+}
+
+const setImg = () => {
+  return sliderImg.setAttribute("src", "./img/" + images[i]);
+};
+
+// const heroImg = document.querySelector(".hero-image");
+// const heroImages = ["hero-1.jpg", "hero-2.jpg", "hero-3.jpg"];
+
+// let j = 0;
+
+// function nextSlide() {
+//   if (j >= heroImages.length - 1) j = -1;
+//   j++;
+//   console.log("HEEEEEEEY");
+//   return setHeroImg();
+// }
+// function setHeroImg() {
+//   return (heroImg.style.backgroundImage = "url('./img/" + heroImages[j] + "')");
+// }
+// setInterval(nextSlide, 10000);
+
+// function submit(event) {
+//   event.preventDefault();
+// }
